4e68ad4bee01d3b70f88e276d5566082958d7a92
MechanicalDeathtrap <korikova-04@mail.ru>
2024-04-19 21:21:06 +0300

    deploying changes

--- src/data/SlideData.js
+++ src/data/SlideData.js
@@ -1,17 +1,20 @@
+import firstImage from "/public/Rectangle_605.png"
+import secondImage from "/public/Rectangle_606.png"
+import thirdImage from "/public/Rectangle_607.png"
 
 export const SliderData = [
     {
-        imgInnerLink: "/public/Rectangle_605.png",
+        imgInnerLink: firstImage,
         title: 'Заголо вок 1 Заголо вок 1 Заголо вок 1',
         description: 'Описание проекта Описание проекта Описание проекта Описание проекта Описание проекта Описание проекта Описание проекта Описание проекта'
     },
     {
-        imgInnerLink: "/public/Rectangle_606.png",
+        imgInnerLink: secondImage,
         title: 'Заголо вок 2 Заголо вок 2 Заголо вок 2',
         description: 'Описание проекта Описание проекта Описание проекта Описание проекта Описание проекта Описание проекта Описание проекта Описание проекта '
     },
     {
-        imgInnerLink: "/public/Rectangle_607.png",
+        imgInnerLink: thirdImage,
         title: 'Заголо вок 3 Заголо вок 3 Заголо вок 3',
         description: 'Описание проекта Описание проекта Описание проекта Описание проекта Описание проекта Описание проекта Описание проекта Описание проекта '
     }
